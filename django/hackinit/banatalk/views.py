8552a27b0d785c385d2015b3957db4c9fa35fd41
jiangchy <jiangchy@shanghaitech.edu.cn>
2017-07-08 21:43:20 +0800

    add index

--- django/hackinit/banatalk/views.py
+++ django/hackinit/banatalk/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, I'm banana")
+
+    context = {
+    
+    }
+    return render(request, 'banatalk/index.html',context)
